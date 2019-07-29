f80a02c544cbd6fd8db2ddba88b0df1771def3d9
Suraj Regmi <regmi125@gmail.com>
2019-07-29 15:31:57 +0545

    First version of the package

---
+++ romanize.py
@@ -0,0 +1,21 @@
+import json
+
+
+with open('characters_map.json') as f:
+    d2r_dict = json.load(f)
+    
+    
+def romanize_text(text):
+    if type(text) == float:
+        return
+    for key,value in d2r_dict.items():
+        text=text.replace(key,value)  
+    return text
+
+def romanize(in_file, out_file):
+    with open(in_file) as f:
+        text = f.read()
+    
+    with open(out_file, 'w+') as f:
+        romanized_text = romanize_text(text)
+        f.write(romanized_text)
